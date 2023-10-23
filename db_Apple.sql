b85e75d71f01f2b73965ae9dcebc190dd4c37866
DigoPires <rodrigo_pires2806@outlook.com>
2023-10-23 20:27:16 -0300

    Commit

--- db_Apple.sql
+++ db_Apple.sql
@@ -11,18 +11,18 @@ create table tb_Produtos(
     descricao varchar(100) not null,
     data_Entrada date not null,
     quantidade decimal(9,2) not null default 0,
-    valor decimal(9,2) not null default 0,
+    valorUnit decimal(9,2) not null default 0,
 
     primary key(cod_prod)
 );
 
--- insert into tb_Produtos(descricao,data_Entrada,quantidade,valor) values(@descricao,@data_Entrada,@quantidade,@valor);
+-- insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values(@descricao,@data_Entrada,@quantidade,@valorUnit);
 
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valor) values("iPhone 13","2023/10/20","12","5499");
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valor) values("Air Pods Pro","2023/10/20","53","1499");
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valor) values("MacBook Air de 15 polegadas","2023/10/20","07","12499");
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valor) values("Apple Watch Ultra 2","2023/05/20","30","9699");
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("iPhone 13","2023/10/20","12","5499");
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("Air Pods Pro","2023/10/20","53","1499");
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("MacBook Air de 15 polegadas","2023/10/20","07","12499");
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("Apple Watch Ultra 2","2023/05/20","30","9699");
 
--- update tb_Produtos set descricao = @descricao, data_Entrada = @data_Entrada, quantidade = @quantidade, valor = @valor where cod_prod = @cod_prod;
+-- update tb_Produtos set descricao = @descricao, data_Entrada = @data_Entrada, quantidade = @quantidade, valorUnit = @valorUnit where cod_prod = @cod_prod;
 
 -- delete * from tb_Produtos where cod_prod = @cod_prod;
