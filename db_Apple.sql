d06be378bcc9e0100670f27ff597b75e08663b75
DigoPires <rodrigo_pires2806@outlook.com>
2023-11-10 14:49:24 -0300

    Commit

--- db_Apple.sql
+++ db_Apple.sql
@@ -11,18 +11,30 @@ create table tb_Produtos(
     descricao varchar(100) not null,
     data_Entrada date not null,
     quantidade decimal(9,2) not null default 0,
-    valorUnit decimal(9,2) not null default 0,
+    valor_unit decimal(9,2) not null default 0,
 
     primary key(cod_prod)
 );
 
--- insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values(@descricao,@data_Entrada,@quantidade,@valorUnit);
+create table tb_Vendas(
+    cod_venda int not null auto_increment,
+    cod_prod int not null,
+    nome_cli varchar(100) not null,
+    data_venda date not null,
+    quantidade decimal(9,2) not null,
+    valor_total decimal(9,2) not null,
 
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("iPhone 13","2023/10/20","12","5499");
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("Air Pods Pro","2023/10/20","53","1499");
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("MacBook Air de 15 polegadas","2023/10/20","07","12499");
-insert into tb_Produtos(descricao,data_Entrada,quantidade,valorUnit) values("Apple Watch Ultra 2","2023/05/20","30","9699");
+    primary key(cod_venda),
+    foreign key(cod_prod) from tb_Produtos(cod_prod)
+)
 
--- update tb_Produtos set descricao = @descricao, data_Entrada = @data_Entrada, quantidade = @quantidade, valorUnit = @valorUnit where cod_prod = @cod_prod;
+-- insert into tb_Produtos(descricao,data_Entrada,quantidade,valor_unit) values(@descricao,@data_Entrada,@quantidade,@valor_unit);
+
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valor_unit) values("iPhone 13","2023/10/20","12","5499");
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valor_unit) values("Air Pods Pro","2023/10/20","53","1499");
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valor_unit) values("MacBook Air de 15 polegadas","2023/10/20","07","12499");
+insert into tb_Produtos(descricao,data_Entrada,quantidade,valor_unit) values("Apple Watch Ultra 2","2023/05/20","30","9699");
+
+-- update tb_Produtos set descricao = @descricao, data_Entrada = @data_Entrada, quantidade = @quantidade, valor_unit = @valor_unit where cod_prod = @cod_prod;
 
 -- delete * from tb_Produtos where cod_prod = @cod_prod;
